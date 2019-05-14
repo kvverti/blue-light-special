391b8968bb8fa96193d5a5417977a9d630198c24
Chris Nero <chrisvn00@gmail.com>
2019-05-14 16:47:45 -0400

    Add block state serialization to multiblockentity

--- src/main/java/io/github/kvverti/bluelightspecial/block/entity/MultiBlockEntity.java
+++ src/main/java/io/github/kvverti/bluelightspecial/block/entity/MultiBlockEntity.java
@@ -1,12 +1,80 @@
 package io.github.kvverti.bluelightspecial.block.entity;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
 import io.github.kvverti.bluelightspecial.BlueLightSpecial;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.command.arguments.BlockStateArgumentType;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.registry.Registry;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class MultiBlockEntity extends BlockEntity {
 
+    private static final Logger log = LogManager.getLogger(MultiBlockEntity.class);
+    private static final BlockStateArgumentType blockStateParser = BlockStateArgumentType.create();
+
+    private final Set<BlockState> containedStates = new HashSet<>();
+
     public MultiBlockEntity() {
         super(BlueLightSpecial.MULTI_BLOCK_ENTITY);
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        tag = super.toTag(tag);
+        ListTag states = new ListTag();
+        for(BlockState bs : containedStates) {
+            states.add(serialize(bs));
+        }
+        tag.put("States", states);
+        return tag;
+    }
+
+    @Override
+    public void fromTag(CompoundTag tag) {
+        super.fromTag(tag);
+        containedStates.clear();
+        ListTag states = tag.getList("States", 8);
+        for(int i = 0; i < states.size(); i++) {
+            String str = states.getString(i);
+            StringReader reader = new StringReader(str);
+            BlockState state;
+            try {
+                state = blockStateParser.parse(reader).getBlockState();
+                containedStates.add(state);
+            } catch(CommandSyntaxException e) {
+                log.error("Could not parse block state string: " + str);
+            }
+        }
+    }
+
+    private StringTag serialize(BlockState state) {
+        StringBuilder sb = new StringBuilder();
+        String blockId = Registry.BLOCK.getId(state.getBlock()).toString();
+        sb.append(blockId).append('[');
+        for(Property<?> prop : state.getProperties()) {
+            sb.append(prop.getName())
+                .append('=')
+                .append(getPropStringValue(state, prop))
+                .append(',');
+        }
+        sb.setCharAt(sb.length() - 1, ']');
+        return new StringTag(sb.toString());
+    }
+
+    private <T extends Comparable<T>> String getPropStringValue(BlockState state, Property<T> prop) {
+        return prop.getValueAsString(state.get(prop));
     }
 }
