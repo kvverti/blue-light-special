0d6b94825c832f1e52e60c8b0fab313ebf3a40b8
Chris Nero <chrisvn00@gmail.com>
2019-05-27 16:12:45 -0400

    Add bounding box for caged lantern

--- src/main/java/io/github/kvverti/bluelightspecial/block/CagedBulbBlock.java
+++ src/main/java/io/github/kvverti/bluelightspecial/block/CagedBulbBlock.java
@@ -3,11 +3,14 @@ package io.github.kvverti.bluelightspecial.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateFactory;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
@@ -35,6 +38,20 @@ public class CagedBulbBlock extends Block {
     @Override
     public int getLuminance(BlockState state) {
         return state.get(POWER);
+    }
+
+    private static final VoxelShape[] BOUNDING_BOXES = {
+        /* DOWN  */ Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 10.0, 11.0),
+        /* UP    */ Block.createCuboidShape(5.0, 6.0, 5.0, 11.0, 16.0, 11.0),
+        /* NORTH */ Block.createCuboidShape(5.0, 5.0, 0.0, 11.0, 11.0, 10.0),
+        /* SOUTH */ Block.createCuboidShape(5.0, 5.0, 6.0, 11.0, 11.0, 16.0),
+        /* WEST  */ Block.createCuboidShape(0.0, 5.0, 5.0, 10.0, 11.0, 11.0),
+        /* EAST  */ Block.createCuboidShape(6.0, 5.0, 5.0, 16.0, 11.0, 11.0)
+    };
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext ctx) {
+        return BOUNDING_BOXES[state.get(ATTACH).getId()];
     }
 
     @Override
